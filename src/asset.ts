61f241e2d48d81c4da395a81c7a1c7d06b8fff4a
Euclid-Twin <wdashuang@gmail.com>
2022-05-26 00:22:54 +0800

    update for refactor

--- src/asset.ts
+++ src/asset.ts
@@ -1,26 +1,75 @@
 import { registerAssetService } from '@soda/soda-asset'
-import axios from 'axios'
-const MAINNET_CHAIN_ID = 137
+import { MessageTypes, sendMessage } from '@soda/soda-core'
+
+const MAINNET_CHAIN_ID = 1
 const retrieveCollections = () => {}
 
 const retrieveAssets = () => {}
 
+const CachedAssetResult = {}
+
 const getNFT = async (metaData: any) => {
-  const chainId = metaData.chainId
-  let url = ''
-  if (Number(chainId) === MAINNET_CHAIN_ID) {
-    url = `https://api.opensea.io/api/v1/asset/${metaData.contract}/${metaData.tokenId}/`
-  } else {
-    url = `https://testnets-api.opensea.io/api/v1/asset/${metaData.contract}/${metaData.tokenId}/`
+  if (metaData.source) {
+    //v1
+    return {
+      ...metaData,
+      chainId: Number(metaData.chainId),
+      contract: metaData.contract,
+      type: 'image',
+      storage: 'ipfs'
+    }
   }
-  const res = await axios.get(url)
-  const result = res.data
+  const req = {
+    type: MessageTypes.InvokeERC721Contract,
+    request: {
+      contract: metaData.contract,
+      method: 'tokenURI',
+      readOnly: true,
+      args: [metaData.tokenId]
+    }
+  }
+  const res: any = await sendMessage(req)
+  // console.log('InvokeERC721Contract: ', res)
+  let source = res.result
+  try {
+    const obj = JSON.parse(source)
+    if (source) {
+      source = obj.image || obj.image_url
+    }
+  } catch (err) {}
   return {
-    ...metaData,
-    source: result.image_url,
+    chainId: Number(metaData.chainId),
+    contract: metaData.contract,
+    tokenId: metaData.tokenId,
+    source,
     type: 'image',
     storage: 'ipfs'
   }
+  // const key = `${metaData.contract}_${metaData.tokenId}`
+  // if (CachedAssetResult[key]) {
+  //   return CachedAssetResult[key]
+  // }
+  // const chainId = metaData.chainId
+  // let url = ''
+  // if (Number(chainId) === MAINNET_CHAIN_ID) {
+  //   url = `https://api.opensea.io/api/v1/asset/${metaData.contract}/${metaData.tokenId}/`
+  // } else {
+  //   url = `https://testnets-api.opensea.io/api/v1/asset/${metaData.contract}/${metaData.tokenId}/`
+  // }
+  // const res = await axios.get(url)
+  // console.log('get opensea NFT: ', res)
+  // const result = res.data
+  // const nft = {
+  //   ...metaData,
+  //   source: result.image_url || result?.asset_contract.image_url,
+  //   type: 'image',
+  //   storage: 'ipfs'
+  // }
+
+  // if (!CachedAssetResult[key]) {
+  //   CachedAssetResult[key] = nft
+  // }
+  // return nft
 }
 
 const init = () => {
